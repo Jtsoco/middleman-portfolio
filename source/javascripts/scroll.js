36368a84bc53ba8c8b8cb232604fa0ae47a85088
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-13 16:57:31 +0900

    added way to check for up or down movement

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -1,11 +1,23 @@
 const sectionElements = document.querySelectorAll(".section");
 console.log(sectionElements);
 sectionElements[0].classList.add("current-page");
-let scrollY = 0;
+let oldScrollY = 0;
+let goUp = false
 sectionElements.forEach((element) => {
   console.log('add')
   element.addEventListener("wheel", function(event) {
-console.log(event);
-console.log('hi');
+    if (element.classList.contains("current-page")) {
+
+    }
   });
 })
+
+window.onscroll = function(e) {
+  if(oldScrollY < window.scrollY){
+    goUp = false;
+  } else {
+      goUp = true;
+  }
+  oldScrollY = window.scrollY
+  console.log(goUp)
+  }
