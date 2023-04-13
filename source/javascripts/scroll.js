e631b22bfe229c8acd62b77c8b68a9fa138bb3b7
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-13 16:48:54 +0900

    for each on each section added

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -1,3 +1,11 @@
 const sectionElements = document.querySelectorAll(".section");
-console.log(sectionElements)
-sectionElements[0].classList.add("current-page")
+console.log(sectionElements);
+sectionElements[0].classList.add("current-page");
+let scrollY = 0;
+sectionElements.forEach((element) => {
+  console.log('add')
+  element.addEventListener("scroll", function(event) {
+console.log(event);
+console.log('hi')
+  })
+})
