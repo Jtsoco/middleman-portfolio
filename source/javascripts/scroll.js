c79d4d8d5e2ab30cf588a4a2d03d3ced4ccf2d2c
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-14 17:38:16 +0900

    have buttons that take you to different sections

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -1,63 +1,52 @@
-const sectionElements = document.querySelectorAll(".section");
-console.log(sectionElements);
-sectionElements[0].classList.add("current-page");
-let oldScrollY = 0;
-let goUp = false
-sectionElements.forEach((element) => {
-  console.log('add')
-  element.addEventListener("wheel", function(event) {
-    if (element.classList.contains("current-page")) {
 
-    }
-  });
+  function smoothScroll(el){
+    console.log(el);
+    el.scrollIntoView(true);
+    console.log('scrolling')
+}
+const sectionOne = document.querySelector("#section-1")
+const sectionTwo = document.querySelector("#section-2")
+const sectionThree = document.querySelector("#section-3")
+
+const aboutBtn = document.querySelector("#about-btn")
+const aboutText = document.querySelector(".about-text")
+const presentationBtn = document.querySelector("#presentation-btn")
+const presentationText = document.querySelector(".presentation-text")
+const projectBtn = document.querySelector("#project-btn")
+const projectText = document.querySelector(".project-text")
+console.log(aboutBtn)
+console.log(aboutText)
+
+aboutBtn.addEventListener("mouseover", function() {
+  aboutText.style.opacity = "1"
 })
-
-window.onscroll = function(e) {
-  if(oldScrollY < window.scrollY){
-    goUp = false;
-  } else {
-      goUp = true;
-  }
-  oldScrollY = window.scrollY
-  let currentPage = document.querySelector(".current-page")
-  scrollCheck();
-  if(justScrolled) {
-    return;
-  }
-  console.log(goUp)
-  justScrolled = true;
-  if (goUp == false && !isLastChild(currentPage)) {
-    currentPage.nextElementSibling.classList.add("current-page");
-    currentPage.classList.remove("current-page");
-    smoothScroll();
-  } else if (goUp == true && !isFirstChild(currentPage)) {
-    currentPage.previousElementSibling.classList.add("current-page");
-    currentPage.classList.remove("current-page");
-    smoothScroll();
-  }
-  // TODO add it so the page moves to the next current page, and current page changes wit this
-  };
-
-  function isLastChild(el) {
-    return (el === el.parentNode.children[el.parentNode.children.length-1])
-  }
-  function isFirstChild(el) {
-    return (el === el.parentNode.children[0])
-  }
-  function smoothScroll(){
-    document.querySelector('.current_page').scrollIntoView({
-        behavior: 'smooth'
-    });
+aboutBtn.addEventListener("mouseout", function() {
+  aboutText.style.opacity = "0"
+})
+aboutBtn.onclick = function() {
+  console.log(sectionOne)
+  smoothScroll(sectionOne)
+}
+// presentation
+presentationBtn.addEventListener("mouseover", function() {
+  presentationText.style.opacity = "1"
+})
+presentationBtn.addEventListener("mouseout", function() {
+  presentationText.style.opacity = "0"
+})
+presentationBtn.onclick = function() {
+  console.log(sectionOne)
+  smoothScroll(sectionTwo)
 }
 
-let timer;
-let justScrolled = false;
-  function scrollCheck() {
-    if(timer) {
-      window.clearTimeout(timer);
-    }
-
-    timer = window.setTimeout(function() {
-      justScrolled = false;
-    }, 50);
-  }
+//project
+projectBtn.addEventListener("mouseover", function() {
+  projectText.style.opacity = "1"
+})
+projectBtn.addEventListener("mouseout", function() {
+  projectText.style.opacity = "0"
+})
+projectBtn.onclick = function() {
+  console.log(sectionOne)
+  smoothScroll(sectionThree)
+}
