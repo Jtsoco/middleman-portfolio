612740320aa93f93cd0c907cb36defba9bd0987a
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-14 09:45:50 +0900

    can switch which section has current page class based on scroll direction

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -20,4 +20,20 @@ window.onscroll = function(e) {
   }
   oldScrollY = window.scrollY
   console.log(goUp)
+  let currentPage = document.querySelector(".current-page")
+  if (goUp == false && !isLastChild(currentPage)) {
+    currentPage.nextElementSibling.classList.add("current-page");
+    currentPage.classList.remove("current-page")
+  } else if (goUp == true && !isFirstChild(currentPage)) {
+    currentPage.previousElementSibling.classList.add("current-page");
+    currentPage.classList.remove("current-page")
+  }
+  // TODO add it so the page moves to the next current page, and current page changes wit this
+  };
+
+  function isLastChild(el) {
+    return (el === el.parentNode.children[el.parentNode.children.length-1])
+  }
+  function isFirstChild(el) {
+    return (el === el.parentNode.children[0])
   }
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,5 @@
 // This is where it all goes :)
-console.log("hi");
+console.log("hi there");
 // const scrollElements = document.querySelectorAll(".card-product");
 // console.log(scrollElements);
 // scrollElements.forEach((el) => {
